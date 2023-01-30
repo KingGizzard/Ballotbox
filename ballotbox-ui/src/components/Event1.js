8fd05bc34e42787333dfde58ed914050f96febdf
Hunter Chen <0x@hunterchen.ca>
2023-01-29 21:24:02 -0500

    file upload skeleton
    trying some different libraries at the moment for file uploading, haven't settled on one yet, will delete the ones I don't use later.

--- ballotbox-ui/src/components/Event1.js
+++ ballotbox-ui/src/components/Event1.js
@@ -1,7 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import { FileUploader } from 'react-drag-drop-files';
+
+const fileTypes = ['jpg', 'png', 'gif', 'txt', 'json', 'webp'];
+
 const Agent1 = () => {
+  const [file, setFile] = useState(null);
+  const [fileData, setFileData] = useState(null);
+  const handleChange = (file) => {
+    setFile(file);
+  }
+
+  useEffect(() => {
+    if (file) {
+      console.log('file: ', file);
+      const reader = new FileReader();
+
+      reader.onabort = () => console.log('file reading was aborted');
+      reader.onerror = () => console.log('file reading has failed');
+      reader.onload = () => {
+        // Do whatever you want with the file contents
+        const binaryStr = reader.result;
+        console.log(binaryStr);
+        setFileData(binaryStr);
+      }
+      reader.readAsArrayBuffer(file);
+    }
+  }, [file]);
+
   return (
     <div>
-      Agent 1
+      <FileUploader 
+        multiple={false}
+        handleChange={handleChange}
+        name="file"
+        types={fileTypes}
+      />
+      <p>{file ? `File info: ${file.name}` : "no files uploaded yet"}</p>
+      {fileData && <img src={fileData} />}
     </div>
   )
 }
--- ballotbox-ui/src/pages/index.js
+++ ballotbox-ui/src/pages/index.js
@@ -1,4 +1,5 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Agent1 from '@/components/Event1';
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
           <p className='font-3xl'>
             stuff goes here
           </p>
+          <Agent1 />
         </div> 
       </main>
     </>
