ba51977e23154487522526ba394d68d99ec420b0
Hunter Chen <0x@hunterchen.ca>
2023-02-04 16:06:05 -0500

    added web3auth

--- ballotbox-ui/src/components/Event1.js
+++ ballotbox-ui/src/components/Event1.js
@@ -2,14 +2,80 @@ import React, { useEffect, useState } from 'react';
 import { FileUploader } from 'react-drag-drop-files';
 import lighthouse from '@lighthouse-web3/sdk'
 
+import { Web3Auth } from "@web3auth/modal";
+import { CHAIN_NAMESPACES } from "@web3auth/base";
+
+import Web3 from 'web3';
+
 const fileTypes = ['txt'];
+
+  
+/*
+We are initializing with EIP155 namespace which will initialize the
+modal with ethereum mainnet by default.
+*/
+
 
 const Agent1 = () => {
   const [file, setFile] = useState(null);
   const [fileData, setFileData] = useState(null);
   const [fileSrc, setFileSrc] = useState(null);
   const [ipfsHash, setIpfsHash] = useState(null);
+  const [txHash, setTxHash] = useState(null);
   const [inputText, setInputText] = useState(null);
+  const [address, setAddress] = useState(null);
+  const [web3, setWeb3] = useState(null);
+  const [provider, setProvider] = useState(null);
+
+  const shortenAddress = (address) => {
+    return address.slice(0, 6) + '...' + address.slice(-4);
+  };
+
+  const web3AuthConnect = async () => {
+    const web3auth = new Web3Auth({
+      chainConfig: {
+        /*
+          you can pass your own chain configs here
+          by default it will only chainId is required for supported networks
+          such as [rinkeby, ropsten, goerli, kovan]
+        */
+        chainNamespace: CHAIN_NAMESPACES.EIP155,
+        chainId: "0xC45",
+        rpcTarget: "https://api.hyperspace.node.glif.io/rpc/v1",
+        displayName: "Filecoin Hyperspace",
+        blockExplorer: "https://hyperspace.filfox.info/en",
+        ticker: "tFIL",
+        tickerName: "Test Filecoin",
+      },
+      clientId: "BGpfvIVo7dEpVFanD8Vw_xBRWSEwININ1VTSBs7GVZPc2vkoNof-B-pCXEPipi3ReT03yQyORxM7D0pwcxHNZH4" // get from https://dashboard.web3auth.io
+    // get from https://dashboard.web3auth.io
+    })
+    await web3auth.initModal();
+    web3auth.connect();
+    setProvider(web3auth.provider);
+  }
+
+  const web3AuthDisconnect = () => {
+    setProvider(null);
+    setAddress(null);
+  }
+
+
+  useEffect(() => {
+    if (provider) {
+      const web3 = new Web3(provider);
+      setWeb3(web3);
+      web3.eth.getAccounts().then((accounts) => {
+        setAddress(accounts[0]);
+      });
+    }
+  }, [provider]);
+
+  useEffect(() => {
+    if (address) {
+      console.log('address: ', address);
+    }
+  }, [address])
 
   const handleChange = (file) => {
     const reader = new FileReader();
@@ -42,7 +108,7 @@ const Agent1 = () => {
 
   return (
     <div className='text-center w-[510px] mx-auto flex flex-col gap-3'>
-      <h1 className='text-2xl'>Agent 1</h1>
+      <h1 className='text-2xl'>Upload Question</h1>
       <div>
         <FileUploader 
           multiple={false}
@@ -77,6 +143,24 @@ const Agent1 = () => {
           {ipfsHash}
         </a>
         <br/>
+        <p>
+          Connect to wallet to upload question to FEVM
+        </p>
+        {!address ?
+            <div
+              className='select-none bg-gray-600 rounded-full px-2 hover:cursor-pointer hover:bg-gray-500 duration-150 ease-in-out w-1/2 mx-auto my-1'
+              onClick={() => web3AuthConnect()}
+            >
+              Connect to wallet
+            </div>
+          :
+            <div
+              className='select-none bg-gray-600 rounded-full px-2 hover:cursor-pointer hover:bg-gray-500 duration-150 ease-in-out w-1/2 mx-auto my-1'
+              onClick={() => web3AuthDisconnect()}
+            >
+              Disconnect from {shortenAddress(address)}
+            </div>
+        }
         <div
           className='select-none bg-gray-600 rounded-full px-2 hover:cursor-pointer hover:bg-gray-500 duration-150 ease-in-out w-1/2 mx-auto my-1'
           onClick={() => setIpfsHash(null)}
--- ballotbox-ui/src/components/Header.js
+++ ballotbox-ui/src/components/Header.js
@@ -1,7 +1,20 @@
+import React from "react";
+
 const Header = () => {
+  const Tab = (props) => {
+    const { title } = props;
+    return ( 
+      <h1 className="rounded-full bg-gray-600 px-4 py-2 text-2xl select-none hover:bg-gray-500 cursor-pointer select-none duration-100 ease-in-out">
+        {title}
+      </h1>
+    )
+  }
+
   return (
-    <header>
-      <h1>Task Tracker</h1>
+    <header className="m-2 flex justify-center gap-4">
+      <Tab title="Post Question" />
+      <Tab title="Answer Questions" />
+      <Tab title="View Answers" />
     </header>
   )
 }
